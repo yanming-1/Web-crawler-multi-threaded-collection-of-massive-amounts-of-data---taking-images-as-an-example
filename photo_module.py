3cc6a541fd3e4c10d81eec2e3d35cad0ebf3e208
iht081 <pcbiht@gmail.com>
2026-04-16 19:06:51 +0800

    Add files via upload
    這是舊的版本

--- photo_module.py
+++ photo_module.py
@@ -1,175 +1,115 @@
-import os
-import threading
-import time
-
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import os
+import threading
+from selenium import webdriver  # selenium 的用法可參見 5-7 節
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
 
 
 def download_pic(url, path):
-    try:
-        response = requests.get(url, timeout=10)
-        response.raise_for_status()
-
-        ext = os.path.splitext(url.split("?")[0])[1]
-        if not ext or len(ext) > 5:
-            ext = ".jpg"
-
-        full_path = path + ext
-        with open(full_path, "wb") as f:
-            f.write(response.content)
-
-        print("下載成功:", full_path)
-    except Exception as e:
-        print("下載失敗:", url, e)
+    pic = requests.get(url)  # 使用 GET 對圖片連結發出請求
+    path += url[url.rfind('.'):]  # 將路徑加上圖片的副檔名
+    f = open(path, 'wb')  # 以指定的路徑建立一個檔案
+    f.write(pic.content)  # 將 HTTP Response 物件的 content寫入檔案中
+    f.close()  # 關閉檔案
 
 
 def get_photolist(photo_name, download_num):
-    photo_list = []
-    url = "https://pixabay.com/zh/"
+    page = 1  
+    photo_list = []  
 
-    option = webdriver.ChromeOptions()
-    option.add_experimental_option("excludeSwitches", ["enable-automation"])
+    url = 'https://pixabay.com/zh/'   # Pixabay 網址
+    option = webdriver.ChromeOptions()  # ←↓加入選項來指定不要有自動控制的訊息
+    option.add_experimental_option('excludeSwitches', ['enable-automation'])
+    browser = webdriver.Chrome(options=option)  # 以指定的選項啟動 Chrome
+    # 連線到pixabay網頁, ●●注意, Chrome出現訊息窗時不可按【停用】鈕, 請按 x 將之關閉, 或不理它也可。
+    browser.get(url)
+    time.sleep(2) 
+    search_box = browser.find_element(By.NAME, 'search') 
+    search_box.send_keys(photo_name)
+    search_box.send_keys(Keys.RETURN)
+    while True:
+        time.sleep(5) 
+        html = browser.page_source
 
-    browser = webdriver.Chrome(options=option)
-
-    try:
-        browser.get(url)
-        time.sleep(2)
-
-        search_box = browser.find_element(By.NAME, "search")
-        search_box.send_keys(photo_name)
-        search_box.send_keys(Keys.RETURN)
-        time.sleep(5)
-
-        while True:
-            html = browser.page_source
-            bs = BeautifulSoup(html, "lxml")
-            img_tags = bs.find_all("img")
-
-            found_new = False
-
-            for img in img_tags:
-                photo = img.get("src", "")
-
-                if not photo:
-                    continue
-                if not photo.startswith("https://"):
-                    continue
-                if "cdn.pixabay.com" not in photo:
-                    continue
-                if photo in photo_list:
-                    continue
-
-                photo_list.append(photo)
-                found_new = True
-                print("抓到圖片:", photo)
-
-                if len(photo_list) >= download_num:
-                    print("已取得足夠圖片:", len(photo_list))
-                    return photo_list
-
-            if not found_new:
-                print("這一頁沒有新圖片了")
-                break
-
-            try:
-                next_link = browser.find_element(By.PARTIAL_LINK_TEXT, "›").get_attribute("href")
-                if not next_link:
-                    break
-
-                browser.get(next_link)
-                time.sleep(3)
-            except Exception:
-                print("沒有下一頁了")
-                break
-
-        return photo_list
-
-    finally:
-        browser.quit()
+    while True:
+        html = browser.page_source
+#        print(html)
+        bs = BeautifulSoup(html, 'lxml')  # 解析網頁
+        #  先尋找標籤為 div, calss 為 'flex_grid ...' 的元素 (這區中才是免費圖庫)
+        #  再尋找所有標籤為 div, calss 為 'item' 的元素
+        photo_item = bs.find('div', {'class': 'flex_grid credits search_results'}).find_all(
+            'div', {'class': 'item'})
+        if len(photo_item) == 0:
+            print('Error, no photo link in page', page)
+            return None
+        for i in range(len(photo_item)):
+            # 尋找標籤 img 並取出 'src' 之中的內容
+            photo = photo_item[i].find('img')['src']
+            if photo == '/static/img/blank.gif':
+                # 尋找標籤 img 並取出 'data-lazy' 之中的內容
+                photo = photo_item[i].find('img')['data-lazy']
+            if photo in photo_list:
+                #                print('photo duplicated in photo_list at page', page, photo)
+                continue
+            #若要下載較高解析度的圖, 可將下行取消註解
+#            photo = photo.replace('_340', '1280')  #更換為1280解析度
+            photo_list.append(photo)  # 將找到的連結新增進 list 之中
+            if len(photo_list) >= download_num:
+                print('end by get photo list size', len(photo_list))
+                browser.close()
+                return photo_list
+        page += 1  # 頁數加1
+        # 找出下一頁的連結網址
+        try:
+            next = browser.find_element_by_partial_link_text(
+                '›').get_attribute('href')
+            browser.get(next)
+        except:  # 沒下一頁了
+            browser.close()
+            return photo_list
 
 
 def create_folder(photo_name):
-    folder_name = input("請輸入要儲存的資料夾名稱: ").strip()
+    folder_name = input("請輸入要儲存的資料夾名稱: ")
 
     if not os.path.exists(folder_name):
         os.mkdir(folder_name)
-        print("資料夾不存在，建立資料夾:", folder_name)
+        print("資料夾不存在, 建立資料夾: " + folder_name)
     else:
-        print("找到資料夾:", folder_name)
+        print("找到資料夾: " + folder_name)
 
-    target_folder = folder_name + os.sep + photo_name
-    if not os.path.exists(target_folder):
-        os.mkdir(target_folder)
-        print("建立資料夾:", photo_name)
+    if not os.path.exists(folder_name + os.sep + photo_name):
+        os.mkdir(folder_name + os.sep + photo_name)
+        print("建立資料夾: " + photo_name)
     else:
         print(photo_name + " 資料夾已存在")
-
     return folder_name
 
 
 def get_photobythread(folder_name, photo_name, photo_list):
-    download_num = len(photo_list)
+    download_num = len(photo_list)  # 設定下載數量為圖片連結串列的長度
+    Q = int(download_num / 100)  # 取商數
+    R = download_num % 100  # 取餘數
 
-    if download_num == 0:
-        print("沒有可下載的圖片")
-        return
-
-    q = download_num // 100
-    r = download_num % 100
-
-    for i in range(q):
+    for i in range(Q):
         threads = []
-
         for j in range(100):
-            index = i * 100 + j
-            t = threading.Thread(
-                target=download_pic,
-                args=(
-                    photo_list[index],
-                    folder_name + os.sep + photo_name + os.sep + str(index + 1),
-                ),
-            )
-            threads.append(t)
-            t.start()
-
-        for t in threads:
-            t.join()
-
-        print(int((i + 1) * 100 / download_num * 100), "%")
+            threads.append(threading.Thread(target=download_pic, args=(
+                photo_list[i*100+j], folder_name + os.sep + photo_name + os.sep + str(i*100+j+1))))
+            threads[j].start()
+        for j in threads:
+            j.join()
+        print(int((i+1)*100/download_num*100), '%')  # 顯示當前進度
 
     threads = []
-    for i in range(r):
-        index = q * 100 + i
-        t = threading.Thread(
-            target=download_pic,
-            args=(
-                photo_list[index],
-                folder_name + os.sep + photo_name + os.sep + str(index + 1),
-            ),
-        )
-        threads.append(t)
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    print("100%")
-
-
-if __name__ == "__main__":
-    photo_name = input("請輸入要搜尋的圖片名稱: ").strip()
-    download_num = int(input("請輸入要下載的圖片數量: "))
-
-    photo_list = get_photolist(photo_name, download_num)
-    print("抓到的圖片數量：", len(photo_list))
-
-    if len(photo_list) > 0:
-        folder_name = create_folder(photo_name)
-        get_photobythread(folder_name, photo_name, photo_list)
-    else:
-        print("沒有抓到任何圖片")
+    for i in range(R):
+        threads.append(threading.Thread(target=download_pic, args=(
+            photo_list[Q*100+i], folder_name + os.sep + photo_name + os.sep + str(Q*100+i+1))))
+        threads[i].start()
+    for i in threads:
+        i.join()
+    print("100%")  # 顯示當前進度
